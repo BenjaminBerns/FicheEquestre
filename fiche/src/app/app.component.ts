ad9ba4d8effa2ad129b32474fe2599bfb88f4afa
Benji <benjixard@gmail.Com>
2025-03-06 15:43:45 +0100

    test version

--- fiche/src/app/app.component.ts
+++ fiche/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { QRCodeComponent } from './qrcode/qrcode.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, QRCodeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'fiche';
+myAngularxQrCode: any;
 }
