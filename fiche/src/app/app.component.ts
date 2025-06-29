831fb0393d2c16440fedda856c18c4b6623265dd
Benji <benjixard@gmail.Com>
2025-06-30 01:57:30 +0200

    front + qrCode + intégrationAPIConnexion

--- fiche/src/app/app.component.ts
+++ fiche/src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
-import {RouterLink, RouterModule, RouterOutlet} from '@angular/router';
-import {NgIf} from '@angular/common';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
+import { NgIf } from '@angular/common';
+import { CompetitionService } from './services/competition.service';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet, RouterLink, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
 
+  // constructor(private competitionService: CompetitionService) {}
+
+  // // Getter propre
+  // get IsConnected(): boolean {
+  //   return this.competitionService.IsConnected;
+  // }
 }
--- fiche/src/app/connexion/connexion.component.ts
+++ fiche/src/app/connexion/connexion.component.ts
@@ -1,6 +1,10 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import bcrypt from 'bcryptjs';
+import { CompetitionService } from '../services/competition.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-connexion',
@@ -10,27 +14,46 @@ import { FormsModule } from '@angular/forms';
   styleUrls: ['./connexion.component.css']
 })
 export class ConnexionComponent {
-
-  constructor(private http: HttpClient) { }
-
+  
   email: string = '';
   password: string = '';
-  user: string = '';
+  user: any = '';
   status: boolean = false;
+  user_id: number = 0;
+  firstname: string = '';
+  surename: string = '';
+  passwordAPI: string = '';
+  role_id: number = 0;
+  created_at: any = null;
+  popUp: boolean = false;
+  
+  constructor(private http: HttpClient, private competitionService: CompetitionService, private router: Router) {}
 
   Email() {
-    this.http.post<{ user: string, status: boolean }>('http://prod-project-32/auth/isEmailExist', { email: this.email })
+    this.http.post<{ status: boolean, user: any }>('http://prod-project-32/auth/isEmailExist', {
+      email: this.email
+    })
       .subscribe({
         next: (response) => {
           console.log('Réponse de l\'API :', response);
           this.status = response.status;
           this.user = response.user;
+          bcrypt.compare(this.password, this.user.password, (err, result) => {
+            if (err) {
+              console.error('Erreur lors de la comparaison des mots de passe', err);
+            } else if (result) {
+              console.log('Le mot de passe est OK');
+              this.popUp = true;
+              this.competitionService.setConnect(this.popUp);
+              this.router.navigate(['/listecompetitions']);
+            } else {
+              console.log('Le mot de passe n\'est pas le bon');
+            }
+          });
         },
         error: (err) => {
           console.error('Erreur API', err);
         }
       });
   }
-
-
 }
